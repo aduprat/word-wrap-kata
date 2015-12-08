6e458b1ff3f1cd95edb7117836a500e56c2e3511
Antoine Duprat <antduprat@gmail.com>
2015-12-08 11:14:44 +0100

    With Guava

--- src/main/java/org/linagora/wordwrap/WordWrap.java
+++ src/main/java/org/linagora/wordwrap/WordWrap.java
@@ -1,8 +1,42 @@
 package org.linagora.wordwrap;
 
+import com.google.common.base.Function;
+import com.google.common.base.Joiner;
+import com.google.common.base.Splitter;
+import com.google.common.base.Strings;
+import com.google.common.collect.FluentIterable;
+
 public class WordWrap {
 
-	public String wrap(String line, int lineLength) {
-		return null;
-	}
+    private static final String SPACE = " ";
+    private static final String SEPARATOR = "\n";
+    private static final String EMPTY_STRING = "";
+
+    public String wrap(String line, int lineLength) {
+        if (Strings.isNullOrEmpty(line)) {
+            return EMPTY_STRING;
+        }
+        if (line.length() < lineLength) {
+            return line;
+        }
+        return Joiner.on(SEPARATOR)
+            .join(removeExtraSpaces(line, lineLength));
+    }
+
+    private Iterable<String> splitLine(String line, int lineLength) {
+        return Splitter.fixedLength(lineLength).split(line);
+    }
+
+    private FluentIterable<String> removeExtraSpaces(String line, int lineLength) {
+        return FluentIterable.from(splitLine(line, lineLength))
+                .transform(new Function<String, String>() {
+
+                    public String apply(String input) {
+                        if (input.endsWith(SPACE)) {
+                            return input.substring(0, input.length() - 1);
+                        }
+                        return input;
+                    }
+                });
+    }
 }
